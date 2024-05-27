6d41c77126bb24b27070345cdac7a3c19fffe7af
Your Name <pablo.r.c@live.com>
2024-05-27 20:52:03 +0200

    added plot_Gen

--- uniplot/__init__.py
+++ uniplot/__init__.py
@@ -1,5 +1,5 @@
 # Add a shortcut such that users of the library can write `from uniplot import plot`
 # instead of `from uniplot.uniplot import plot`.
-from uniplot.uniplot import plot, plot_to_string, histogram
+from uniplot.uniplot import plot, plot_to_string, histogram, plot_gen
 
-__all__ = ["plot", "plot_to_string", "histogram"]
+__all__ = ["plot", "plot_to_string", "plot_gen", "histogram"]
